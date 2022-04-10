5229e71cefe823023c81d533b23c976af82f9f4a
RamyaMandapati <101368541+RamyaMandapati@users.noreply.github.com>
2022-04-10 14:12:50 -0700

    adding searchHotels Api

--- api/controllers/hotels.controller.js
+++ api/controllers/hotels.controller.js
@@ -45,21 +45,8 @@ class HotelsController {
             res.status(400).send({success:false, message})
         }
     }
-    //Search hotel APi
-    static async searchHotels (req, res) {
-        try {
-            const body = req.body;
-            const schema = Joi.object({
-                term: Joi.string().required(),
-            });
-            await schema.validateAsync(body);
-            const reports = await HotelsService.searchHotels(req.body);
-            res.status(200).send(reports);
-        } catch (e) {
-            const message = e.message || 'Error occurred while searching hotels.'
-            res.status(400).send({message})
-        }
-    }
+
+  
     static async addBooking(req, res) {
         try {
             const schema = Joi.object({
